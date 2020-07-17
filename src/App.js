7d89d737d2c3276fe150a41577812ea799466c96
Victor Lin <victor.s.lin@gmail.com>
2020-07-16 20:38:51 -0700

    Debug: reorder import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
 
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
-
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 const App = () => (
   <div>
